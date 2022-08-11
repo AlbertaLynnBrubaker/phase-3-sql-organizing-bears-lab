7e8d8cfd9d86f1ca1a4922dfb3f2dc8af555aee7
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-10 21:47:17 -0500

    commit tree 9e2a08639e36a3fefa44631aac695cce97392e52

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Brown', 'Delicious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Brown', 'Rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Black', 'Fiesty', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 11, 'M', 'Brown', 'Stern', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'F', 'Brown', 'Shy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'Green', 'He''s a mean one', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'White', 'Mother', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 12, 'M', 'Brown', 'Delicious', 1);
